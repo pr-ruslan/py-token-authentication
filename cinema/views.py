0c9dfec2bdecc7c44a74fe341b37246ebe3d6347
Ruslan Priadko <r.priadko@ukr.net>
2025-09-26 13:51:33 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 
 from django.db.models import F, Count
-from django.http import Http404
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import mixins
 
 from cinema.models import (
